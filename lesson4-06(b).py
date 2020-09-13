9eca86b8981b7f9fe0f95fce65287759e3681b82
Евгений Подхалюзин <70483255+Podhaluzin@users.noreply.github.com>
2020-09-14 00:37:25 +0300

    Переделал задание 6(b)
    Получилось как-то так.

--- lesson4-06(b).py
+++ lesson4-06(b).py
@@ -1,5 +1,11 @@
-from itertools import cycle
-
-my_list = [True, 'ABC', 123, None]
-for el in cycle(my_list):
-    print(el)
+user_choice = input('Нажмите ENTER, для запуска скрипта')
+if user_choice == '':
+    from itertools import cycle
+    my_list = [True, 'ABC', 123, None]
+    i = 0
+    for el in cycle(my_list):
+        if i == 12:
+            break
+        else:
+            print(el)
+            i += 1  
