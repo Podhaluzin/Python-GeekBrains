52e564696d29436209cac35a016a657570c8737f
Евгений Подхалюзин <70483255+Podhaluzin@users.noreply.github.com>
2020-09-13 19:39:11 +0300

    Delete DZ06.py

--- DZ06.py
+++
@@ -1,9 +0,0 @@
-a = float(input("Введите первое значение: "))
-b = float(input("Введите второе значение: "))
-day = 1
-if a > b:
-    print(day)
-while a < b:
-    a = a + a/10
-    day += 1
-print(day)
